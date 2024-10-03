207f850ec9315936ba999c1e38fbcc8752edbde5
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-10-03 21:01:51 +0800

    update koneksi

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
     $server = "localhost";
     $user = "root";
     $pass = "";
-    $database = "database_pemesananmakanan";
+    $database = "database_absensi";
 
     $koneksi = mysqli_connect($server,$user,$pass,$database) or die(mysqli_error($koneksi));
 ?>
